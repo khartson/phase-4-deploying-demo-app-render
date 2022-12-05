eb0b1eae442679cb787546ef61f408ae603f909c
Kyle <khartson2017@gmail.com>
2022-12-05 11:53:31 -0500

    changed build script

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -2,7 +2,6 @@
 # exit on error
 set -o errexit
 
-
 # Add build commands for front end
 rm -rf public
 npm install --prefix client && npm run build --prefix client
@@ -10,4 +9,3 @@ cp -a client/build/. public/
 
 bundle install
 bundle exec rake db:migrate 
-bundle exec rake db:seed
